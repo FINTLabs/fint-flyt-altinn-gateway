60e7d16b8982404302c1c3f13299860c794ae5a2
Ole Anders Eidjord <ole.anders.eidjord@vigoiks.no>
2025-02-12 15:51:55 +0100

    ARKIV-317 ip

--- src/main/java/no/fintlabs/flyt/IncomingInstanceMappingService.java
+++ src/main/java/no/fintlabs/flyt/IncomingInstanceMappingService.java
@@ -37,7 +37,7 @@ public class IncomingInstanceMappingService implements InstanceMapper<KafkaAltin
         return Flux.fromIterable(Arrays.asList("ref-data-as-pdf", "dom-forelegg", "beskrivelse-yrkestransportloven", "politiattest-foretak", "politiattest-dagligleder"))
                 .map(documentReference ->
                         webClient.get()
-                                .uri(String.format("http://fint-altinn-service:8080/beta/api/file/%s/%s", incomingInstance.getInstanceId(), documentReference))
+                                .uri(String.format("http://10.104.2.202:8080/beta/api/file/%s/%s", incomingInstance.getInstanceId(), documentReference))
                                 .exchangeToMono(response ->
                                         response.bodyToMono(byte[].class)
                                                 .map(body ->
