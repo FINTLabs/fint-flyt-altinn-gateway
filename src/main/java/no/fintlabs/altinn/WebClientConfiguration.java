96605761782a58e58cff00df8e40b637620e31b4
Nils-Odd Solberg <nilsodd@fintlabs.no>
2025-06-26 11:56:29 +0200

    ARKIV-317 More memory to the people

--- src/main/java/no/fintlabs/altinn/WebClientConfiguration.java
+++ src/main/java/no/fintlabs/altinn/WebClientConfiguration.java
@@ -3,17 +3,26 @@ package no.fintlabs.altinn;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfiguration {
+
+    private static final int MAX_IN_MEMORY_SIZE = 50 * 1024 * 1024;
 
     @Value("${fint.altinn.api.base-url}")
     private String altinnApiBaseUrl;
 
     @Bean
     public WebClient webClient() {
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(clientCodecConfigurer ->
+                        clientCodecConfigurer.defaultCodecs().maxInMemorySize(MAX_IN_MEMORY_SIZE))
+                .build();
+
         return WebClient.builder()
+                .exchangeStrategies(strategies)
                 .baseUrl(altinnApiBaseUrl)
                 .build();
     }
